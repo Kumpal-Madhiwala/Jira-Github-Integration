d4a93caf24f8cad1baf930be0367b3c66bd4fc54
Laxman Sooriyathas <laxman.sooriyathas@tophatmonocle.com>
2017-11-22 13:19:04 -0500

    use android column names

--- column.py
+++ column.py
@@ -1,18 +1,18 @@
 class Column:
-    TO_DO = "Ready for Dev"
-    IN_PROGRESS = "Dev"
-    CODE_REVIEW = "Dev Review"
-    QA_REVIEW = "Done"
+    TO_DO = "Open"
+    IN_PROGRESS = "In Progress"
+    CODE_REVIEW = "Code Review"
+    QA_REVIEW = "QA"
 
     @staticmethod
     def from_string(column_string):
-        if column_string == "Ready for Dev":
+        if column_string == "Open":
             return Column.TO_DO
-        elif column_string == "Dev":
+        elif column_string == "In Progress":
             return Column.IN_PROGRESS
-        elif column_string == "Dev Review":
+        elif column_string == "Code Review":
             return Column.CODE_REVIEW
-        elif column_string == "Done":
+        elif column_string == "QA":
             return Column.QA_REVIEW
         else:
             return None
