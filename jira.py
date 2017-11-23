261dee5ef8a2080981592fdb5955dd4dfecc4784
Laxman Sooriyathas <laxman.sooriyathas@tophatmonocle.com>
2017-11-23 11:00:09 -0500

    turn jira wrapper into class in order to optimize

--- jira.py
+++ jira.py
@@ -1,128 +1,113 @@
-#Here are the wrapper methods for JIRA
 import requests
+import os
 
 from column import Column
 
-from column import Column
+class Jira:
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Basic {}".format(os.environ['jira_token'])
+    }
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Basic {}".format(os.environ['jira_token'])
-}
+    def __init__(self, ticket):
+        self.ticket = ticket
+        self.payload = self.get_payload()
 
-# jira tickets have a transition from To Do to In Progress called 'Start'
-# which doesn't map to a column. If a ticket is in To Do and you want to move
-# it to In Progress, call this method
-def start_ticket(ticket_number):
-    move_to_column(ticket_number, "Start")
+    # jira tickets have a transition from To Do to In Progress called 'Start'
+    # which doesn't map to a column. If a ticket is in To Do and you want to move
+    # it to In Progress, call this method
+    def start_ticket(self):
+        self.move_to_column("Start")
 
-def move_to_column(ticket_number, column):
-    #some definition goes here
-    transition_id = get_transition_id_from_column_name(ticket_number, column)
-    payload = {
-        "transition": {
-            "id": str(transition_id)
+    def move_to_column(self, column):
+        #some definition goes here
+        transition_id = self.get_transition_id_from_column_name(column)
+        payload = {
+            "transition": {
+                "id": str(transition_id)
+            }
         }
-    }
+        url = self.create_base_url("transitions")
+        r = requests.post(
+            url,
+            json=payload,
+            headers=Jira.headers
+        )
+        return r.status_code
 
-    url = create_base_url(ticket_number, "transitions")
+    def update_status(self, **kwargs):
+        # TODO: remove default assignee
+        assignee = kwargs.pop('assignee', 'kumpal.madhiwala')
+        fix_version = kwargs.pop('fix_version', None)
 
-    r = requests.post(
-        url,
-        json=payload,
-        headers=headers
-    )
-    #Handle Errors here (If not found etc....)
-    return r.status_code
+        self.set_assignee(assignee)
+        if fix_version:
+            self.add_fix_version(fix_version)
 
-def update_status(ticket_number, **kwargs):
-    # TODO: remove default assignee
-    assignee = kwargs.pop('assignee', 'kumpal.madhiwala')
-    fix_version = kwargs.pop('fix_version', None)
+    def get_column(self):
+        return Column.from_string(self.payload['fields']['status']['name'])
 
-    set_assignee(ticket_number, assignee)
-    if fix_version:
-        add_fix_version(ticket_number, fix_version)
-
-def set_assignee(ticket_number, assignee):
-    payload = {
-        "name": assignee
-    }
-
-    url = create_base_url(ticket_number, "assignee")
-
-    response = requests.put(
-        url,
-        json=payload,
-        headers=headers
-    )
-    return response.status_code
-
-def get_transition_id_from_column_name(ticket_number, column_name):
-    url = create_base_url(ticket_number, "transitions")
-    response = requests.get(
-        url,
-        headers=headers
-    )
-    data = response.json()
-    for transition in data["transitions"]:
-        if transition["name"] == column_name:
-            return transition["id"]
-
-def get_column(ticket_number):
-    url = create_base_url(ticket_number, '')
-
-    response = requests.get(
-        url,
-        headers=headers
-    )
-    data = response.json()
-
-    return Column.from_string(data['fields']['status']['name'])
-
-def add_fix_version(ticket_number, fix_version):
-    url = create_base_url(ticket_number, "")
-    payload = {
-        "update": {
-            "fixVersions": [
-                {
-                    "add": {
-                        "name": fix_version
+    def add_fix_version(self, fix_version):
+        url = self.create_base_url()
+        payload = {
+            "update": {
+                "fixVersions": [
+                    {
+                        "add": {
+                            "name": fix_version
+                        }
                     }
-                }
-            ]
+                ]
+            }
         }
-    }
+        r = requests.put(
+            url,
+            json=payload,
+            headers=Jira.headers
+        )
+        return r.status_code
 
-    r = requests.put(
-        url,
-        json=payload,
-        headers=headers
-    )
+    def needs_product_review(self):
+        labels = self.payload['fields']['labels']
+        return "needs_product_review" in labels
 
-    return r.status_code
 
-def needs_product_review(ticket_number):
-    url = create_base_url(ticket_number, "")
-    response = requests.get(
-        url,
-        headers = headers
-    )
+    def get_ticket_type(self):
+        return self.payload['fields']['issuetype']['name']
 
-    r = response.json()
-    labels = r['fields']['labels']
+    # Private Methods
 
-    return "needs_product_review" in labels
+    def get_payload(self):
+        url = self.create_base_url()
+        response = requests.get(
+            url,
+            headers=Jira.headers
+        )
+        return response.json()
 
-def create_base_url(ticket_number, field):
-    return "https://github-jira-integration.atlassian.net/rest/api/2/issue/{0}/{1}".format(ticket_number, field)
+    def get_transition_id_from_column_name(self, column_name):
+        url = self.create_base_url("transitions")
+        response = requests.get(
+            url,
+            headers=Jira.headers
+        )
+        data = response.json()
+        for transition in data["transitions"]:
+            if transition["name"] == column_name:
+                return transition["id"]
 
-def get_ticket_type(ticket_number):
-    url = create_base_url(ticket_number, '')
+    def set_assignee(self, assignee):
+        payload = {
+            "name": assignee
+        }
+        url = self.create_base_url("assignee")
+        response = requests.put(
+            url,
+            json=payload,
+            headers=Jira.headers
+        )
+        return response.status_code
 
-    response = requests.get(
-        url,
-        headers=headers
-    )
-    data = response.json()
-    return data['fields']['issuetype']['name']
+    def create_base_url(self, field=""):
+        return "https://github-jira-integration.atlassian.net/rest/api/2/issue/{0}/{1}".format(self.ticket, field)
+
